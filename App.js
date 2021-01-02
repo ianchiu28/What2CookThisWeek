be3f6338cf853a37fc90d3630f7e15c2913388b7
Ian Chiu <nk9710632@gmail.com>
2021-01-02 14:00:09 +0800

    feat: practice scroll view

--- App.js
+++ App.js
@@ -2,6 +2,48 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, ScrollView, Image, TextInput, Button } from 'react-native';
 
+// const logo = {
+//   uri: 'https://reactnative.dev/img/tiny_logo.png',
+//   width: 64,
+//   height: 64
+// };
+
+// export default App = () => (
+//   <ScrollView>
+//     <Text style={{ fontSize: 96 }}>Scroll me plz</Text>
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Text style={{ fontSize: 96 }}>If you like</Text>
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Text style={{ fontSize: 96 }}>Scrolling down</Text>
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Text style={{ fontSize: 96 }}>What's the best</Text>
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Text style={{ fontSize: 96 }}>Framework around?</Text>
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Image source={logo} />
+//     <Text style={{ fontSize: 96 }}>React Native</Text>
+//   </ScrollView>
+// )
+
 // const PizzaTransfer = () => {
 //   const [text, setText] = useState('');
 //   return (
