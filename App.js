108a57f4ca90326ca5ee8cd45cfcad45afe9769c
Ian Chiu <nk9710632@gmail.com>
2021-01-02 14:09:33 +0800

    feat: practice list view

--- App.js
+++ App.js
@@ -1,6 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, ScrollView, Image, TextInput, Button } from 'react-native';
+import { FlatList, StyleSheet, Text, View, ScrollView, Image, TextInput, Button } from 'react-native';
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     paddingTop: 22
+//   },
+//   item: {
+//     padding: 10,
+//     fontSize: 18,
+//     height: 44
+//   }
+// });
+
+// const FlatListBasics = () => (
+//   <View style={styles.container}>
+//     <FlatList
+//       data={[
+//         {key: 'Devin'},
+//         {key: 'Dan'},
+//         {key: 'Dominic'},
+//         {key: 'Jackson'},
+//         {key: 'James'},
+//         {key: 'Joel'},
+//         {key: 'John'},
+//         {key: 'Jillian'},
+//         {key: 'Jimmy'},
+//         {key: 'Julie'}
+//       ]}
+//       renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+//     />
+//   </View>
+// );
+
+// export default FlatListBasics;
 
 // const logo = {
 //   uri: 'https://reactnative.dev/img/tiny_logo.png',
