d76564cacb3005c0d0caa404f46ef078669a676a
Ian Chiu <nk9710632@gmail.com>
2021-01-02 13:55:30 +0800

    feat: hide old test code

--- App.js
+++ App.js
@@ -2,88 +2,88 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, ScrollView, Image, TextInput, Button } from 'react-native';
 
-const PizzaTransfer = () => {
-  const [text, setText] = useState('');
-  return (
-    <View style={{padding: 10}}>
-      <TextInput
-        style={{height: 40}}
-        placeholder='Type here to translate!'
-        onChangeText={text => setText(text)}
-        defaultValue={text}
-      />
-      <Text style={{padding: 10, fontSize: 42}}>
-        {text.split(' ').map(word => word && '🍕').join(' ')}
-      </Text>
-    </View>
-  );
-};
+// const PizzaTransfer = () => {
+//   const [text, setText] = useState('');
+//   return (
+//     <View style={{padding: 10}}>
+//       <TextInput
+//         style={{height: 40}}
+//         placeholder='Type here to translate!'
+//         onChangeText={text => setText(text)}
+//         defaultValue={text}
+//       />
+//       <Text style={{padding: 10, fontSize: 42}}>
+//         {text.split(' ').map(word => word && '🍕').join(' ')}
+//       </Text>
+//     </View>
+//   );
+// };
 
-const Cat = (props) => {
-  const [isHungry, setIsHungry] = useState(true);
+// const Cat = (props) => {
+//   const [isHungry, setIsHungry] = useState(true);
 
-  return (
-    <View>
-      <Text>I am {props.name}, and I am {isHungry ? 'hungry' : 'full'}!</Text>
-      <Button
-        onPress={() => {
-          setIsHungry(false);
-        }}
-        disabled={!isHungry}
-        title={isHungry ? 'Pour me some milk, please!' : 'Thank you!'}
-      />
-    </View>
-  );
-};
+//   return (
+//     <View>
+//       <Text>I am {props.name}, and I am {isHungry ? 'hungry' : 'full'}!</Text>
+//       <Button
+//         onPress={() => {
+//           setIsHungry(false);
+//         }}
+//         disabled={!isHungry}
+//         title={isHungry ? 'Pour me some milk, please!' : 'Thank you!'}
+//       />
+//     </View>
+//   );
+// };
 
-const getFullName = (firstName, secondName, thirdName) => {
-  return firstName + ' ' + secondName + ' ' + thirdName;
-}
+// const getFullName = (firstName, secondName, thirdName) => {
+//   return firstName + ' ' + secondName + ' ' + thirdName;
+// }
 
-export default function App() {
-  return (
-    <ScrollView>
-      <Text>Some text</Text>
-      <View>
-        <Text>Some more text</Text>
-        <Image
-          source={{
-            uri: 'https://reactnative.dev/docs/assets/p_cat2.png'
-          }}
-          style={{ width: 200, height: 200 }}
-        />
-      </View>
-      <TextInput
-        style={{
-          height: 40,
-          borderColor: 'gray',
-          borderWidth: 1
-        }}
-        defaultValue='You can type in me'
-      />
-      <Text>Hello, I am {getFullName('Rum', 'Tum', 'Tugger')}</Text>
-      <View>
-        <Text>Hello, I am...</Text>
-        <TextInput
-          style={{
-            height: 40,
-            borderColor: 'gray',
-            borderWidth: 1
-          }}
-          defaultValue='Name me!'
-        />
-      </View>
-      <Cat name='Maru' />
-      <Cat name='Spot' />
-      <PizzaTransfer />
-    </ScrollView>
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    //   <Text>Try Edit me!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
-  );
-}
+// export default function App() {
+//   return (
+//     <ScrollView>
+//       <Text>Some text</Text>
+//       <View>
+//         <Text>Some more text</Text>
+//         <Image
+//           source={{
+//             uri: 'https://reactnative.dev/docs/assets/p_cat2.png'
+//           }}
+//           style={{ width: 200, height: 200 }}
+//         />
+//       </View>
+//       <TextInput
+//         style={{
+//           height: 40,
+//           borderColor: 'gray',
+//           borderWidth: 1
+//         }}
+//         defaultValue='You can type in me'
+//       />
+//       <Text>Hello, I am {getFullName('Rum', 'Tum', 'Tugger')}</Text>
+//       <View>
+//         <Text>Hello, I am...</Text>
+//         <TextInput
+//           style={{
+//             height: 40,
+//             borderColor: 'gray',
+//             borderWidth: 1
+//           }}
+//           defaultValue='Name me!'
+//         />
+//       </View>
+//       <Cat name='Maru' />
+//       <Cat name='Spot' />
+//       <PizzaTransfer />
+//     </ScrollView>
+//     // <View style={styles.container}>
+//     //   <Text>Open up App.js to start working on your app!</Text>
+//     //   <Text>Try Edit me!</Text>
+//     //   <StatusBar style="auto" />
+//     // </View>
+//   );
+// }
 
 // const styles = StyleSheet.create({
 //   container: {
